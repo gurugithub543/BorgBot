7d545e819b114258f51001a9fbcd7599a11f4d6a
prabal0071 <55538824+prabal0071@users.noreply.github.com>
2019-09-20 20:03:00 +0530

    Update quickheal.py

--- stdplugins/quickheal.py
+++ stdplugins/quickheal.py
@@ -44,7 +44,7 @@ async def _(event):
             "`Quick Heal Total Security Checkup\n\n\nSubscription: Pro User\nValid Until: 31/12/2099\n\nFile Scanned... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
             "`Quick Heal Total Security Checkup\n\n\nSubscription: Pro User\nValid Until: 31/12/2099\n\nFile Scanned... 84%\n█████████████████████▒▒▒▒ `",
             "`Quick Heal Total Security Checkup\n\n\nSubscription: Pro User\nValid Until: 31/12/2099\n\nFile Scanned... 100%\n█████████████████████████ `",
-            "`Quick Heal Total Security Checkup\n\n\nSubscription: Pro User\nValid Until: 31/12/2099\n\nTask: 01 of 01 Files Scanned...\n\nReault: No Virus Found...`"
+            "`Quick Heal Total Security Checkup\n\n\nSubscription: Pro User\nValid Until: 31/12/2099\n\nTask: 01 of 01 Files Scanned...\n\nResult: No Virus Found...`"
         ]
 
         for i in animation_ttl:
@@ -83,7 +83,7 @@ async def _(event):
             "`Quick Heal Total Security Checkup\n\n\nSubscription: Pro User\nValid Until: 31/12/2099\n\nFile Scanned... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
             "`Quick Heal Total Security Checkup\n\n\nSubscription: Pro User\nValid Until: 31/12/2099\n\nFile Scanned... 84%\n█████████████████████▒▒▒▒ `",
             "`Quick Heal Total Security Checkup\n\n\nSubscription: Pro User\nValid Until: 31/12/2099\n\nFile Scanned... 100%\n█████████████████████████ `",
-            "`Quick Heal Total Security Checkup\n\n\nSubscription: Pro User\nValid Until: 31/12/2099\n\nTask: 01 of 01 Files Scanned...\n\nReault: No Virus Found...`"
+            "`Quick Heal Total Security Checkup\n\n\nSubscription: Pro User\nValid Until: 31/12/2099\n\nTask: 01 of 01 Files Scanned...\n\nResult: No Virus Found...`"
         ]
 
         for i in animation_ttl:
